7af4319e265cbf363d0f2a769cb7192575452e2f
Lukas Jungmann <lukas.jungmann@oracle.com>
2017-02-10 22:04:40 +0000

    update test/test project
    svn path=/trunk/; revision=307

--- istack-commons/maven-plugin/src/test/script/setproxy.groovy
+++ istack-commons/maven-plugin/src/test/script/setproxy.groovy
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -56,6 +56,7 @@ if (settings?.proxies) {
             new Node(p, "port", proxy.port)
             if (proxy.username) {new Node(p, "username", proxy.username)}
             if (proxy.password) {new Node(p, "password", proxy.password)}
+            if (proxy.id) {new Node(p, "id", proxy.id)} else {new Node(p, "id", proxy.protocol)}
             new Node(p, "host", proxy.host)
             new Node(p, "active", proxy.active)
             new Node(p, "nonProxyHosts", proxy.nonProxyHosts)
